e2ae2d3b4328e66a7af29b7151c30063725bf965
Jesus <jgarcia28599@gmail.com>
2022-10-18 21:04:39 -0300

    Upload image

--- scripts/catalogo.js
+++ scripts/catalogo.js
@@ -29,16 +29,48 @@ const cargarDatos = async () => {
                                 <div>Stock: ${product.stock}</div>
                                 <div><img src="${product.img}" class="img-fluid" alt="${product.name}"></img></div>
                             </a>
-                            <button type="button" class="btn btn-outline-primary" id="editInfoProd" onclick="editarInfo('${product.name}')">Editar informacion</button>
-                            <button type="button" class="btn btn-outline-primary" id="addImageProd">Agregar imagen</button>
+                            <button data-bs-toggle="collapse" data-bs-target="#editInfoCollapse" class="btn btn-outline-primary" id="editInfoProd">Editar informacion</button>
+                            <button data-bs-toggle="collapse" data-bs-target="#changeImageCollapse" class="btn btn-outline-primary" id="addImageProd">Agregar imagen</button>
+
+                            <div id="editInfoCollapse" class="collapse">
+                                <form id="editInfoProdForm" method="put">
+
+                                <div class="form-floating mb-3 d-flex">
+                                    <input type="text" class="form-control" id="newNameProd${product._id}" name="newNameProd" placeholder="Nombre">
+                                    <label for="newNameProd">Nombre</label>
+                                    <button class="w-50 btn btn-lg btn-primary" id="editNameProd${product._id}" type="button" onclick="editarInfo('${product.name}')">Editar</button>
+                                </div>
+
+                                <div class="form-floating mb-3 d-flex">
+                                    <input type="number" class="form-control" id="newPrizeProd${product._id}" name="newPrizeProd" placeholder="Precio">
+                                    <label for="newPrizeProd">Precio</label>
+                                    <button class="w-50 btn btn-lg btn-primary" id="editPrizeProd${product._id}" type="button" onclick="editarInfo('${product.name}')">Editar</button>
+                                </div>
+
+                                <div class="form-floating mb-3 d-flex">
+                                    <input type="number" class="form-control" id="newStockProd${product._id}" name="newStockProd" placeholder="Stock">
+                                    <label for="newStockProd">Stock</label>
+                                    <button class="w-50 btn btn-lg btn-primary" id="editStockProd${product._id}" type="button" onclick="editarInfo('${product.name}')">Editar</button>
+                                </div>
+
+                                </form>
+                            </div>
+
+                            <div id="changeImageCollapse" class="collapse">
+                                <form id="changeImageForm" method="put">
+                                    <div class="m-3">
+                                        <label for="changeImageProd" class="form-label">Elegir imagen</label>
+                                        <input class="form-control mb-2" type="file" id="changeImageProd${product._id}" name="changeImageProd">
+                                        <button class="w-75 btn btn-lg btn-primary" id="changeImage${product._id}" type="button" onclick="cambiarImagen('${product.name}')">Subir imagen</button>
+                                    </div>
+                                </form>
+                            </div>
                         </div>
                     </span>
                     `)
                     )
 
                     HTMLResponse.innerHTML += template.join('');
-
-                    editarInfo();
 
                 })
         } else {
@@ -78,12 +110,165 @@ const editarInfo = (n) => {
 
     productos.forEach(element => {
         if (element.dataset.name === n) {
-            console.log(`ID: ${element.dataset.id}`);
+
+            let id = element.dataset.id;
+            let editarNombre = document.querySelector(`#editNameProd${id}`)
+            let editarPrecio = document.querySelector(`#editPrizeProd${id}`)
+            let editarStock = document.querySelector(`#editStockProd${id}`)
+
+            editarNombre.addEventListener('click', editarNombreProd);
+            editarNombre.myParamId = id;
+
+            editarPrecio.addEventListener('click', editarPrecioProd);
+            editarPrecio.myParamId = id;
+
+            editarStock.addEventListener('click', editarStockProd);
+            editarStock.myParamId = id;
+
         }
     });
+}
+
+const editarNombreProd = (id) => {
+
+    id = id.currentTarget.myParamId
+    let name = document.querySelector(`#newNameProd${id}`);
+    const jwt = sessionStorage.getItem('jwt');
+
+    fetch(`http://localhost:8080/api/products/${id}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+            xtoken: jwt
+        },
+        body: JSON.stringify({
+            name: name.value.toUpperCase()
+        })
+    })
+        .then(response => response.json())
+        .then(data => {
+
+            window.location.href = "file:///G:/PP/Frontend/templates/catalogo.html"
+
+        })
+        .catch((err) => {
+            console.log(err);
+
+        })
 
 
 }
+
+const editarPrecioProd = (id) => {
+
+    id = id.currentTarget.myParamId
+    let prize = document.querySelector(`#newPrizeProd${id}`);
+    const jwt = sessionStorage.getItem('jwt');
+
+    fetch(`http://localhost:8080/api/products/${id}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+            xtoken: jwt
+        },
+        body: JSON.stringify({
+            prize: prize.value
+        })
+    })
+        .then(response => response.json())
+        .then(data => {
+
+            window.location.href = "file:///G:/PP/Frontend/templates/catalogo.html"
+
+        })
+        .catch((err) => {
+            console.log(err);
+
+        })
+
+
+}
+
+const editarStockProd = (id) => {
+
+    id = id.currentTarget.myParamId
+    let stock = document.querySelector(`#newStockProd${id}`);
+    const jwt = sessionStorage.getItem('jwt');
+
+    fetch(`http://localhost:8080/api/products/${id}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+            xtoken: jwt
+        },
+        body: JSON.stringify({
+            stock: stock.value
+        })
+    })
+        .then(response => response.json())
+        .then(data => {
+
+            window.location.href = "file:///G:/PP/Frontend/templates/catalogo.html"
+
+        })
+        .catch((err) => {
+            console.log(err);
+
+        })
+
+}
+
+const cambiarImagen = (n) => {
+
+    let contenedor = document.querySelector('#catalogue');
+    let productos = contenedor.querySelectorAll('span')
+
+    productos.forEach(element => {
+        if (element.dataset.name === n) {
+
+            const jwt = sessionStorage.getItem('jwt');
+            let id = element.dataset.id;
+            let changeImageButton = document.querySelector(`#changeImage${id}`)
+            let newImageData = document.querySelector(`#changeImageProd${id}`)
+            let data = new FormData();
+            data.append('archivo', newImageData.files[0])
+
+            let API_URL = `http://localhost:8080/api/uploads/products/${id}`
+
+            changeImageButton.addEventListener('click',
+
+                fetch(API_URL, {
+                    method: 'PUT',
+                    headers: {
+                        xtoken: jwt
+                    },
+                    body: data
+                })
+                    .then(response => response.json())
+                    .then(data => {
+
+                        window.location.href = "file:///G:/PP/Frontend/templates/catalogo.html"
+
+                    })
+                    .catch((err) => {
+                        console.log(err);
+
+                    })
+
+            )
+
+            /*
+            if(changeImageButton.value){
+                console.log(API_URL)
+            } else {
+                console.log('nao nao manito')
+            }
+            */
+
+        }
+    });
+}
+
 
 
 const capitalizar = (string) => {
